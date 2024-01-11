1b0a611fce836517304b7a41590caa5ef3d67b79
unknown <vitoralves6589@gmail.com>
2024-01-11 14:57:21 -0300

    Projeto finalizado

---
+++ teste.js
@@ -0,0 +1,11 @@
+let listaGenerica = [];
+//
+let linguagemDeProgramação = ['JavaScript','C','C++', 'Kotlin' , 'Python'];
+//
+linguagemDeProgramação.push('Java', 'Ruby' , 'GoLang');
+//
+console.log(linguagemDeProgramação[0]);
+console.log(linguagemDeProgramação[1]);
+console.log(linguagemDeProgramação[2]);
+//ou
+console.log(linguagemDeProgramação[-1]);
